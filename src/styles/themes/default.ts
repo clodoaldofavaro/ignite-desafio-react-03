b639dea3724f5edea1f40590de461c139c0cdd4f
Clodoaldo Favaro-Bling <clodoaldo.favero@bling.com.br>
2023-07-24 21:52:50 -0300

    initial setup

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+import { tokens } from './tokens'
+
+export const defaultTheme = {
+  'base-title': tokens.white,
+  'base-subtitle': tokens['gray-100'],
+  'base-text': tokens['gray-200'],
+  'base-span': tokens['gray-300'],
+  'base-label': tokens['gray-400'],
+  'base-border': tokens['gray-500'],
+  'base-post': tokens['gray-600'],
+  'base-profile': tokens['gray-700'],
+  'base-background': tokens['gray-800'],
+  'base-input': tokens['gray-900'],
+  'base-blue': tokens['blue-100'],
+} as const
