4c0c4cb3367b8831831b8b3624294cd1446d4fec
Clodoaldo Favaro-Bling <clodoaldo.favero@bling.com.br>
2023-07-24 22:01:18 -0300

    feat: add app basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import './App.css'
 
 export function App() {
   return (
