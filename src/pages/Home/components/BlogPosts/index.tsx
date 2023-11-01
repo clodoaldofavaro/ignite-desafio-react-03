c7b90846df673de1d44327ac66bd85da4f1530f2
Clodoaldo Favaro-Bling <clodoaldo.favero@bling.com.br>
2023-11-01 19:23:40 -0300

    feat: show post last update date on card

--- src/pages/Home/components/BlogPosts/index.tsx
+++ src/pages/Home/components/BlogPosts/index.tsx
@@ -2,11 +2,15 @@ import { useContextSelector } from 'use-context-selector'
 import { BlogPostsContext } from '../../../../contexts/BlogPostsContext'
 import { BlogPostsContainer } from './styles'
 import Markdown from 'react-markdown'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export function BlogPosts() {
   const blogPosts = useContextSelector(BlogPostsContext, (context) => {
     return context.blogPosts
   })
+
+  console.log(blogPosts)
 
   return (
     <BlogPostsContainer>
@@ -16,10 +20,18 @@ export function BlogPosts() {
             <div className="post-card" key={blogPost.number}>
               <header>
                 <h2>{blogPost.title}</h2>
+                <span>
+                  {formatDistanceToNow(new Date(blogPost.updatedAt), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </span>
               </header>
               <main>
                 <Markdown>
-                  {blogPost.body.substring(0, 300).concat('...')}
+                  {blogPost.body.length > 300
+                    ? blogPost.body.substring(0, 300).concat('...')
+                    : blogPost.body}
                 </Markdown>
               </main>
             </div>
